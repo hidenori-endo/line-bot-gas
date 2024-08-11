694daf38456bb0a409d4f54ab08101c07d58e94c
hidenori-endo <nintendwu@gmail.com>
2024-08-12 03:43:13 +0900

    use json mode

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
 import os
-import time
-from xml.etree import ElementTree as ET
+import re
 
 import requests
-from fake_useragent import UserAgent
+from GoogleNews import GoogleNews
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
+from openai import OpenAI
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 LINE_ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
@@ -17,40 +17,67 @@ MAX_RETRIES = 5
 RETRY_DELAY = 15
 
 
-def get_rss_titles_and_urls(rss_url):
-    for attempt in range(MAX_RETRIES):
-        try:
-            ua = UserAgent()
-            header = {"User-Agent": str(ua.chrome)}
-            response = requests.get(rss_url, headers=header)
-            response.raise_for_status()
+def get_news():
+    UNNECESSARY_COMMON = "'\"\’\’”{}\[\]「」『』【】・|^\.|^\．|^\…"
+    UNNECESSARY_PATTERN = rf"[\s|\u3000|{UNNECESSARY_COMMON}]"
+    PARENTHESES_PATTERN = r"（.*?）|［.*?］|〈.*?〉"
 
-            root = ET.fromstring(response.content)
-            channel = root.find("channel")
+    googlenews = GoogleNews(lang="ja", region="JP")
+    googlenews.set_topic("CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtcGhHZ0pLVUNnQVAB")
+    googlenews.get_news()
 
-            titles = []
-            shortened_urls = []
+    # news_textとnews_linkの順番を保持するため、辞書に格納する
+    news_data = {}
+    for text, link in zip(googlenews.get_texts(), googlenews.get_links()):
+        text_without_parentheses = re.sub(PARENTHESES_PATTERN, "", text)
+        cleaned_text = re.sub(UNNECESSARY_PATTERN, "", text_without_parentheses)
+        # 重複を避けるため、cleaned_textをキーとして辞書に格納
+        news_data[cleaned_text] = link
 
-            for item in channel.findall("item"):
-                title = item.find("title").text
-                print(title)
+    request_news = ""
+    cnt = 0
+    for text, link in news_data.items():
+        request_news += f"{cnt},{text}\n"
+        cnt += 1
 
-                original_url = item.find("link").text
-                shortened_url = shorten_url(original_url)
+    phrases_prompt = f"""
+    ```
+    [{request_news}]
+    ```
+    公認会計士の業務に役立つと思われる内容、すなわち会計基準や企業の粉飾決算、内部統制や不正、監査、財務に関係するニュースがあれば、番号を抽出して。
+    短期的な企業業績や株価、スキャンダルなどに関するものは抽出しないで。
+    該当するものがなかった場合は空のリストで出力して
+    """
+    return (phrases_prompt, list(news_data.keys()), list(news_data.values()))
 
-                titles.append(title)
-                shortened_urls.append(shortened_url)
-
-            return titles, shortened_urls
-        except requests.exceptions.RequestException as e:
-            print(f"Error fetching RSS feed (Attempt {attempt+1}/{MAX_RETRIES}): {e}")
-            if attempt < MAX_RETRIES - 1:
-                print(f"Retrying in {RETRY_DELAY} seconds...")
-                time.sleep(RETRY_DELAY)
-            else:
-                print("Max retries reached. Giving up.")
-                return None, None
-
+def chat_gpt(prompt):
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    response = client.chat.completions.create(
+        model="gpt-4o-2024-08-06",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant.",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        response_format={
+            "type": "json_schema",
+            "json_schema": {
+                "name": "news_response",
+                "strict": True,
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "id": {"type": "array", "items": {"type": "string"}}
+                    },
+                    "required": ["id"],
+                    "additionalProperties": False,
+                },
+            },
+        },
+    )
+    return response.choices[0].message.content
 
 def shorten_url(url):
     api_url = f"http://tinyurl.com/api-create.php?url={url}"
@@ -63,50 +90,29 @@ def shorten_url(url):
         return url  # エラー時は元のURLを返す
 
 
-def chat_gpt(number_title):
-    api_url = "https://api.openai.com/v1/chat/completions"
-    messages = [
-        {
-            "role": "system",
-            "content": "公認会計士の業務に役立つと思われる内容、すなわち会計基準や企業の粉飾決算、内部統制や監査、財務に関係するニュースがあれば、タイトルとURLを抽出して。企業の業績や株価、スキャンダルなどに関するものは抽出しないで。該当するものがなかった場合は「関連するニュースはありません」という文字列のみ出力して",
-        },
-        {"role": "user", "content": number_title},
-    ]
-    headers = {
-        "Authorization": f"Bearer {OPENAI_API_KEY}",
-        "Content-type": "application/json",
-    }
-    data = {
-        "model": "gpt-4o",
-        "max_tokens": 2048,
-        "temperature": 0.9,
-        "messages": messages,
-    }
-    response = requests.post(api_url, headers=headers, data=json.dumps(data))
-    response.raise_for_status()
-    response_text = response.json()["choices"][0]["message"]["content"]
-    return response_text
-
-
 def send_line(message):
     line_bot_api = LineBotApi(LINE_ACCESS_TOKEN)
     line_bot_api.push_message(LINE_GROUP_ID, TextSendMessage(text=message))
 
 def main(request):
-    rss_url = "https://news.google.com/news/rss/headlines/section/topic/BUSINESS?hl=ja&gl=JP&ceid=JP:ja"
-    titles, shortened_urls = get_rss_titles_and_urls(rss_url)
+    prompt, titles, links = get_news()
+    # print(prompt)
+    # print("--------------------")
+    response_text = chat_gpt(prompt)
+    print(response_text)
 
-    if titles is None or shortened_urls is None:
-        print("Failed to fetch RSS feed. Exiting.")
-        return
+    try:
+        response_json = json.loads(response_text)
+        selected_ids = [int(id) for id in response_json["id"]]
+        selected_titles_and_links = [
+            (titles[i], links[i]) for i in selected_ids if 0 <= i < len(titles)
+        ]
 
-    articles = []
-    number_title = ""
-
-    for i, (title, url) in enumerate(zip(titles, shortened_urls)):
-        articles.append([url, title])
-        number_title += f"{i+1}. {title}\n"
-
-    response_text = chat_gpt(number_title)
-    if "関連するニュースはありません" not in response_text:
-        send_line(response_text)
+        # LINEに送信するメッセージを作成
+        message = ""
+        for title, link in selected_titles_and_links:
+            message += f"{title}\n{shorten_url(link)}\n\n"
+        send_line(message)
+        # print(message)
+    except (json.JSONDecodeError, KeyError) as e:
+        print(f"Error: {e}")
